f2e6da41beba1536d635e076c3dab81f8d25ccb4
AmeyTonpe <101514346+coderamey@users.noreply.github.com>
2022-04-03 00:34:31 +0530

    Create menu.h

---
+++ 3_implementation/inc/menu.h
@@ -0,0 +1,12 @@
+
+
+ #ifndef __MENU_H__
+ #define __MENU_H__
+
+/**
+ * Functions that displays the menu, where the user has two options - to play a new game/ exit the application
+ * 
+ */
+ void displayMenu();
+
+ #endif //__MENU_H__
